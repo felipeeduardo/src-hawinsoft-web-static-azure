dc843faa8d0e47f0da80a1ebdcf4c6c613231e6a
Felipe Eduardo <f.l.p.eduardo@hotmail.com>
2019-06-10 00:42:33 -0300

    update project

--- src/components/Page/Private/Home/Home.vue
+++ src/components/Page/Private/Home/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <v-container grid-list-md text-xs-center>
-    <card-group></card-group>
-    <card-download></card-download>
+    <v-layout row wrap class="py-3 px-2">
+      <v-flex d-flex xs12 sm6 md6 class="px-1 py-1">
+        <card-product/>
+      </v-flex>
+      <v-flex d-flex xs12 sm6 md6 class="px-1 py-1">
+        <card-product/>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-import CardGroup from "@/components/organisms/Card/CardGroup";
-import CardDownload from "@/components/organisms/Card/CardDownload";
+import CardProduct from "@/components/organisms/Card/CardProduct";
 export default {
   components: {
-    CardGroup,
-    CardDownload
+    CardProduct
   }
 };
 </script>
---
+++ src/components/organisms/Card/CardProduct/CardProduct.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card class="elevation-3">
+    <v-img
+      class="white--text"
+      height="200px"
+      contain
+      :src="require('@/assets/img/hawinsoft-rpa-banner.png')"
+    ></v-img>
+    <v-card-title>
+      <span class="title font-weight-light">Robotic process automation</span>
+    </v-card-title>
+
+    <v-list two-line subheader>
+      <v-list-tile v-for="item in items" :key="item.title" avatar @click>
+        <v-list-tile-avatar>
+          <v-icon :class="[item.iconClass]">{{item.icon}}</v-icon>
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title>My space</v-list-tile-title>
+          <v-list-tile-sub-title>...</v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          icon: "person",
+          iconClass: "primary lighten-1 white--text",
+          title: "My space",
+          subtitle: "..."
+        },
+        {
+          icon: "folder",
+          iconClass: "grey lighten-1 white--text",
+          title: "Recipes",
+          subtitle: "Jan 17, 2014"
+        },
+        {
+          icon: "folder",
+          iconClass: "grey lighten-1 white--text",
+          title: "Work",
+          subtitle: "Jan 28, 2014"
+        }
+      ]
+    };
+  }
+};
+</script>
