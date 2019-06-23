3db58b32b575ace623176c6fb3abfbe6ba3647ab
Felipe Eduardo <f.l.p.eduardo@hotmail.com>
2019-06-23 17:57:55 -0300

    include https

--- app.js
+++ app.js
@@ -4,15 +4,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3800));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('/*', function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] == 'https') {
-        res.sendFile(__dirname + '/dist/index.html')
-    } else {
-        next();
-    }
+app.get('/*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
 });
 
 var server = app.listen(app.get('port'), function () {
     var port = server.address().port;
-    console.log(' Server running... in ' + port)
+    console.log(' Server running... in ' + port);
 })
