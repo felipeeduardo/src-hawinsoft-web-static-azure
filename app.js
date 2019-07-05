86f77a0d51f91a8b6155739b6ef448b67f2a47ed
Felipe Eduardo <f.l.p.eduardo@hotmail.com>
2019-07-05 11:30:20 -0300

    include https

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var secure = require('express-force-https');
 var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 3800));
+app.use(secure);
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/*', function (req, res) {
     res.sendFile(__dirname + '/dist/index.html')
