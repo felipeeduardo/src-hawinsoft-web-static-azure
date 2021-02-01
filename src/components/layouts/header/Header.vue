14219dca0439be4f4e60099976bf5d676702b54c
Felipe Eduardo <f.l.p.eduardo@hotmail.com>
2021-02-01 14:29:03 -0300

    Ajustes para edição em nomes de pastas

---
+++ src/components/layouts/header/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-toolbar app dark color="primary">
+    <v-toolbar-side-icon>
+      <v-img :src="require('@/assets/img/hawinsoft-white.png')" contain height="30"></v-img>
+    </v-toolbar-side-icon>
+    <v-toolbar-title class="headline text-uppercase" exact>
+      <span class="font-weight-light">HAWINSOFT</span>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <menu-header></menu-header>
+  </v-toolbar>
+</template>
+
+<script>
+import MenuHeader from "@/components/organisms/menu/private/menuHeader";
+export default {
+  components: {
+    MenuHeader
+  }
+};
+</script>
